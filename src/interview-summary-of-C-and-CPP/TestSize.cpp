f1c78884400dd2661e861623a2791d941d776e86
haoran119 <34557994+haoran119@users.noreply.github.com>
2021-08-10 13:35:52 +1000

    Create TestSize.cpp

---
+++ src/interview-summary-of-C-and-CPP/TestSize.cpp
@@ -0,0 +1,28 @@
+class TestSize1
+{
+public:
+    TestSize1 () : a (0) {}
+    virtual void F () = 0;
+private:
+    int a;
+};
+class TestSize2 : public TestSize1
+{
+public:
+    TestSize2 () : b (1) {}
+    virtual void F () { b = 3; }
+private:
+    int b;
+};
+int main(int argc, char* argv[])
+{
+    printf ("size of TestSize2 = %d", sizeof (TestSize2));
+    return 0;
+}
+
+/*
+What will be printed, and why?
+
+In 32-bit environment, size of TestSize2 = 12.
+12 = TestSize2 virtual table pointer(4) + TestSize2::b(4) + TestSize1::a(4)
+*/
