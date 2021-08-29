ccecd93657a68a626845bbbc12bc96008615a6c0
haoran119 <34557994+haoran119@users.noreply.github.com>
2021-08-29 19:37:09 +1000

    Update isPalindrome.cpp

--- 面试总结之C-C++/isPalindrome.cpp
+++ 面试总结之C-C++/isPalindrome.cpp
@@ -1,46 +1,52 @@
+/*
+判断一个字符串是不是回文串
+注意给出两种定义：一种是严格回文串，完美匹配；另一种只考虑数字字母，且大小写不敏感
+LeetCode 125. Valid Palindrome
+    http://www.cnblogs.com/pegasus923/p/5612541.html
+reverse - C++ Reference
+    http://www.cplusplus.com/reference/algorithm/reverse/
+isalnum - C++ Reference
+    http://www.cplusplus.com/reference/cctype/isalnum/?kw=isalnum
+toupper - C++ Reference
+    http://www.cplusplus.com/reference/cctype/toupper/
+*/
+
 //
 //  main.cpp
-//  LeetCode
+//  LEETCODE
 //
-//  Created by Hao on 2017/3/16.
-//  Copyright © 2017年 Hao. All rights reserved.
+//  Created by Hao on 2020/8/29.
+//  Copyright © 2020年 Hao. All rights reserved.
 //
 
 #include <iostream>
-#include <string>
-#include <algorithm>    // reverse
-#include <cctype>       // toupper, isalnum
 using namespace std;
 
 class Solution
 {
 public:
     // Reverse the whole string
-    int isPalindrome(const string& str)
+    bool isPalindrome(const string &str)
     {
-        if (str.empty()) return false;
-        
+        if (str.empty())
+            return false;
+
         string sTemp = str;
-        
-        cout << sTemp << endl;
-        
-        // STL algorithm
+
         reverse(sTemp.begin(), sTemp.end());
-        
-        cout << sTemp << endl;
-        
+
         return (sTemp == str);
     }
-    
+
     // Considering only alphanumeric characters and ignoring cases
-    int isPalindrome2(const string& str)
+    bool isPalindrome2(const string &str)
     {
-        if (str.empty()) return false;
-        
+        if (str.empty())
+            return false;
+
         size_t start = 0, end = str.size() - 1;
-        
-        while (start < end)
-        {
+
+        while (start < end) {
             if (! isalnum(str.at(start)))
                 ++ start;
             else if (! isalnum(str.at(end)))
@@ -52,55 +58,34 @@ public:
                 -- end;
             }
         }
-        
+
         return true;
     }
 };
 
 int main(int argc, char* argv[])
 {
-    Solution    testSolution;
-    
+    Solution    mySolution;
+
     auto words = {"A man, a plan, a canal: Panama", "race a car", "", "a b a"};
-    
+
+    // 0
+    // 0
+    // 0
+    // 1
     for (auto word : words)
-        cout << testSolution.isPalindrome(word) << endl;
+        cout << mySolution.isPalindrome(word) << endl;
 
     cout << endl;
-    
+
+    // 1
+    // 0
+    // 0
+    // 1
     for (auto word : words)
-        cout << testSolution.isPalindrome2(word) << endl;
+        cout << mySolution.isPalindrome2(word) << endl;
+
+    cout << endl;
 
     return 0;
 }
-
-/*
-判断一个字符串是不是回文串
-
-注意给出两种定义：一种是严格回文串，完美匹配；另一种只考虑数字字母，且大小写不敏感 
-LeetCode 125. Valid Palindrome
-http://www.cnblogs.com/pegasus923/p/5612541.html
-toupper - C++ Reference
-http://www.cplusplus.com/reference/cctype/toupper/
-isalnum - C++ Reference
-http://www.cplusplus.com/reference/cctype/isalnum/?kw=isalnum
-reverse - C++ Reference
-http://www.cplusplus.com/reference/algorithm/reverse/
-
-A man, a plan, a canal: Panama
-amanaP :lanac a ,nalp a ,nam A
-0
-race a car
-rac a ecar
-0
-0
-a b a
-a b a
-1
-
-1
-0
-0
-1
-Program ended with exit code: 0
-*/
