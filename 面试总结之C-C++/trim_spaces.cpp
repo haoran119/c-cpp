960b9f59a3193b67e2aba4d51e4f8300aab5b66b
haoran119 <34557994+haoran119@users.noreply.github.com>
2021-09-04 23:27:35 +1000

    Update trim_spaces.cpp

--- 面试总结之C-C++/trim_spaces.cpp
+++ 面试总结之C-C++/trim_spaces.cpp
@@ -1,14 +1,24 @@
+/*
+删除字符串的首尾多余空格
+利用STL方法，记得处理string::npos。执行结果注意string::npos的值。
+注意对空格字符串输入“ ”，输出仍是“ ”，只删除首尾多余空格。
+string::find_first_not_of - C++ Reference
+    http://www.cplusplus.com/reference/string/string/find_first_not_of/
+string::npos - C++ Reference
+    http://www.cplusplus.com/reference/string/string/npos/
+*/
+
 //
 //  main.cpp
 //  LeetCode
 //
-//  Created by Hao on 2017/3/16.
-//  Copyright © 2017年 Hao. All rights reserved.
+//  Created by Hao on 2021/09/01.
+//  Copyright © 2021年 Hao. All rights reserved.
 //
 
 #include <iostream>
-#include <string>
-#include <cstddef>        // std::size_t
+// #include <string>
+// #include <cstddef>        // std::size_t
 using namespace std;
 
 class Solution
@@ -16,10 +26,8 @@ class Solution
 public:
     void TrimSpaces(string& ioStr)
     {
-        size_t  startPos = ioStr.find_first_not_of(" ");
-        size_t  endPos = ioStr.find_last_not_of(" ");
-
-        cout << startPos << "\n" << endPos << endl;
+        size_t startPos = ioStr.find_first_not_of(" ");
+        size_t endPos = ioStr.find_last_not_of(" ");
 
         // npos is a static member constant value with the greatest possible value for an element of type size_t.
         // This constant is defined with a value of -1, which because size_t is an unsigned integral type, it is the largest possible representable value for this type.
@@ -27,8 +35,6 @@ public:
             startPos = 0;
         if (endPos == string::npos)
             endPos = 0;
-
-        cout << startPos << "\n" << endPos << endl;
 
         ioStr = ioStr.substr(startPos, endPos - startPos + 1);
     }
@@ -40,41 +46,18 @@ int main ()
 
     string  str[5] = {"  abc     ", "abc     ", "    abc", " ", ""};
 
+    /*
+    "abc"
+    "abc"
+    "abc"
+    " "
+    ""
+    */
     for (auto i = 0; i < 5; i ++)
     {
         testSolution.TrimSpaces(str[i]);
-        cout << "\"" << str[i] << "\"" << "\n" << endl;
+        cout << "\"" << str[i] << "\"" << endl;
     }
 
     return 0;
 }
-
-/*
-删除字符串的首尾多余空格
-
-利用STL方法，记得处理string::npos。
-注意对空格字符串输入“ ”，输出仍是“ ”，只删除首尾多余空格。
-string::find_first_not_of - C++ Reference
-http://www.cplusplus.com/reference/string/string/find_first_not_of/
-string::npos - C++ Reference
-http://www.cplusplus.com/reference/string/string/npos/
-
-执行结果注意string::npos的值。
-4
-4
-"abc"
-2
-2
-"abc"
-6
-6
-"abc"
-18446744073709551615
-0
-" "
-18446744073709551615
-0
-""
-
-Program ended with exit code: 0
-*/
