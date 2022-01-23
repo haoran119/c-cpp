4155e18fa47933b2ccb83a9369fce178d550732b
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-01-23 18:11:55 +1100

    Create Input and Output.cpp

---
+++ 面试总结之C-C++/HackerRank/Input and Output.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <iostream>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a, b, c;
+    
+    std::cin >> a >> b >> c;
+    
+    std::cout << a + b + c << std::endl;
+    
+    return 0;
+}
