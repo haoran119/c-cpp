f7581942429fa7221348256400c1a5e6f1e76334
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-12-03 20:44:36 +1100

    Update and rename TrimSpaces.cpp to trim_spaces.cpp

--- 面试总结之C-C++/trim_spaces.cpp
+++ 面试总结之C-C++/trim_spaces.cpp
@@ -26,11 +26,12 @@ class Solution
 public:
     void TrimSpaces(string& ioStr)
     {
-        size_t startPos = ioStr.find_first_not_of(" ");
-        size_t endPos = ioStr.find_last_not_of(" ");
+        auto startPos = ioStr.find_first_not_of(" ");
+        auto endPos = ioStr.find_last_not_of(" ");
 
         // npos is a static member constant value with the greatest possible value for an element of type size_t.
-        // This constant is defined with a value of -1, which because size_t is an unsigned integral type, it is the largest possible representable value for this type.
+        // This constant is defined with a value of -1, which because size_t is an unsigned integral type, 
+        // it is the largest possible representable value for this type.
         if (startPos == string::npos)
             startPos = 0;
         if (endPos == string::npos)
